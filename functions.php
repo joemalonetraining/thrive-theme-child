f227c546c071c12f44c007b6d7fd32512b344bfc
joemalonetraining <ceo@joemalonetraining.com>
2026-05-11 12:36:54 -0500

    Fix Landing Test template styling

--- functions.php
+++ functions.php
@@ -12,3 +12,24 @@ add_action('wp_enqueue_scripts', function () {
         wp_get_theme()->get('Version')
     );
 });
+
+add_action('wp_enqueue_scripts', function () {
+    if (! is_page_template('page-landing-test.php')) {
+        return;
+    }
+
+    wp_enqueue_style(
+        'jm-landing-test',
+        get_stylesheet_directory_uri() . '/assets/css/landing-test.css',
+        ['child-style'],
+        filemtime(get_stylesheet_directory() . '/assets/css/landing-test.css')
+    );
+
+    wp_enqueue_script(
+        'jm-landing-test',
+        get_stylesheet_directory_uri() . '/assets/js/landing-test.js',
+        [],
+        filemtime(get_stylesheet_directory() . '/assets/js/landing-test.js'),
+        true
+    );
+}, 99);
